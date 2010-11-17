052e8d412104c6ff7c75eecb5213238f435b0303
Patrick Perry <patperry@gmail.com>
2010-11-17 15:22:12 -0500

    Add more IEEE754 utils

--- libla/ieee754.c
+++ libla/ieee754.c
@@ -2,9 +2,24 @@
 # include <config.h>
 #endif
 
-#include <stdint.h>
+#include <float.h>
+#include <math.h>
+#include <glib.h>
 #include <libla/ieee754.h>
 
+
+#define DBL_EXPMASK            ((uint16_t) 0x7FF0)
+#define DBL_SIGNMASK           ((uint16_t) 0x8000)
+#define DBL_EXPBIAS            ((uint16_t) 0x3FE0)
+#define DBL_EXPBIAS_INT32      ((uint32_t) 0x7FF00000)
+#define DBL_MANTISSAMASK_INT32 ((uint32_t) 0x000FFFFF); /* for the MSB only */
+#if G_BYTE_ORDER == G_BIG_ENDIAN
+# define DBL_EXPPOS_INT16 0
+# define DBL_SIGNPOS_BYTE 0
+#else
+# define DBL_EXPPOS_INT16 3
+# define DBL_SIGNPOS_BYTE 7
+#endif
 
 union double_uint64 {
     double d;
@@ -18,4 +33,128 @@ la_identical (double x,
     union double_uint64 ux = { x };
     union double_uint64 uy = { y };
     return ux.w == uy.w;
+}
+
+/* ported from tango/math/IEEE.d */
+double
+la_nextup (double x)
+{
+    union double_uint64 ps = { x };
+
+    if ((ps.w & 0x7FF0000000000000ULL) == 0x7FF0000000000000ULL) {
+        /* First, deal with NANs and infinity */
+        if (x == -INFINITY) return -DBL_MAX;
+        return x; // +INF and NAN are unchanged.
+    }
+    if (ps.w & 0x8000000000000000ULL)  { /* Negative number */
+        if (ps.w == 0x8000000000000000ULL) { /* it was negative zero */
+            ps.w = 0x0000000000000001ULL; /* change to smallest subnormal */
+            return ps.d;
+        }
+        --ps.w;
+    } else { /* Positive number */
+        ++ps.w;
+    }
+    return ps.d;
+}
+
+/* ported from tango/math/IEEE.d */
+double
+la_nextdown (double x)
+{
+    return -nextup(-x);
+}
+
+/* ported from tango/math/IEEE.d */
+double
+la_ieeemean (double x,
+             double y)
+{
+    if (!((x>=0 && y>=0) || (x<=0 && y<=0))) return NAN;
+
+    union double_uint64 ul;
+    union double_uint64 xl = { x };
+    union double_uint64 yl = { y };
+    uint64_t m = ( (xl.w & 0x7FFFFFFFFFFFFFFFULL)
+                 + (yl.w & 0x7FFFFFFFFFFFFFFFULL) ) >> 1;
+    m |= (xl.w & 0x8000000000000000ULL);
+    ul.w = m;
+
+    return ul.d;
+}
+
+double
+la_mknan (uint64_t payload)
+{
+    union double_uint64 x = { NAN };
+
+    /* get sign, exponent, and quiet bit from NAN */
+    x.w &= 0xFFF8000000000000ULL;
+
+    /* ignore sign, exponent, and quiet bit in payload */
+    payload &= 0x0007FFFFFFFFFFFFULL;
+    x.w |= payload;
+
+    return x.d;
+}
+
+uint64_t
+la_getnan (double x)
+{
+    union double_uint64 payload = { x };
+
+    /* clear sign, exponent, and quiet bit */
+    payload.w &= 0x0007FFFFFFFFFFFFULL;
+    return payload.w;
+}
+
+int
+la_feqrel (double x,
+           double y)
+{
+    /* Public Domain. Original Author: Don Clugston, 18 Aug 2005.
+     * Ported to C by Patrick Perry, 26 Feb 2010.
+     */
+    if (x == y) return DBL_MANT_DIG; /* ensure diff!= 0, cope with INF. */
+
+    double diff = fabs(x - y);
+
+    union { double r; uint16_t w[sizeof(double)/2]; } pa = { x };
+    union { double r; uint16_t w[sizeof(double)/2]; } pb = { y };
+    union { double r; uint16_t w[sizeof(double)/2]; } pd = { diff };
+
+    /* The difference in abs(exponent) between x or y and abs(x-y)
+     * is equal to the number of significand bits of x which are
+     * equal to y. If negative, x and y have different exponents.
+     * If positive, x and y are equal to 'bitsdiff' bits.
+     * AND with 0x7FFF to form the absolute value.
+     * To avoid out-by-1 errors, we subtract 1 so it rounds down
+     * if the exponents were different. This means 'bitsdiff' is
+     * always 1 lower than we want, except that if bitsdiff==0,
+     * they could have 0 or 1 bits in common.
+     */
+    int bitsdiff = (( ((pa.w[DBL_EXPPOS_INT16] & DBL_EXPMASK)
+                     + (pb.w[DBL_EXPPOS_INT16] & DBL_EXPMASK)
+                     - ((uint16_t) 0x8000 - DBL_EXPMASK)) >> 1)
+                   - (pd.w[DBL_EXPPOS_INT16] & DBL_EXPMASK)) >> 4;
+
+    if ((pd.w[DBL_EXPPOS_INT16] & DBL_EXPMASK) == 0) {
+        /* Difference is denormal
+         * For denormals, we need to add the number of zeros that
+         * lie at the start of diff's significand.
+         * We do this by multiplying by 2^DBL_MANT_DIG
+         */
+        pd.r *= (1 / DBL_EPSILON);
+
+        return (bitsdiff + DBL_MANT_DIG
+                - (pd.w[DBL_EXPPOS_INT16] >> 4));
+    }
+
+    if (bitsdiff > 0)
+        return bitsdiff + 1; /* add the 1 we subtracted before */
+
+    /* Avoid out-by-1 errors when factor is almost 2. */
+    return (bitsdiff == 0
+            && !((pa.w[DBL_EXPPOS_INT16]
+                  ^ pb.w[DBL_EXPPOS_INT16]) & DBL_EXPMASK)) ? 1 : 0;
 }
--- libla/ieee754.h
+++ libla/ieee754.h
@@ -2,8 +2,17 @@
 #ifndef _LA_IEEE754_H
 #define _LA_IEEE754_H
 
-int la_identical (double x,
-                  double y);
+#include <stdint.h>
 
+int      la_identical (double   x,
+                       double   y);
+double   la_nextup    (double   x);
+double   la_nextdown  (double   x);
+double   la_ieeemean  (double   x,
+                       double   y);
+double   la_mknan     (uint64_t payload);
+uint64_t la_getnan    (double   x);
+int      la_feqrel    (double   x,
+                       double   y);
 
-#endif /* _LA_IEEE74_H */
+#endif /* _LA_IEEE754_H */
