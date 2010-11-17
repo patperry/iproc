c966276ed3d55e4be645e36c7fd539d841e6bdbd
Patrick Perry <patperry@gmail.com>
2010-11-17 14:24:53 -0500

    Add la_indentical

---
+++ libla/ieee754.c
@@ -0,0 +1,20 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdint.h>
+#include <libla/ieee754.h>
+
+
+union double_uint64 {
+    double d;
+    uint64_t w;
+};
+
+int
+identical (double x, double y)
+{
+    union double_uint64 ux = { x };
+    union double_uint64 uy = { y };
+    return ux.w == uy.w;
+}
---
+++ libla/ieee754.h
@@ -0,0 +1,9 @@
+
+#ifndef _LA_IEEE754_H
+#define _LA_IEEE754_H
+
+int la_identical (double x,
+                  double y);
+
+
+#endif /* _LA_IEEE74_H */
