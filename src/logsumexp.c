31e3afa1a81b53c87d93bdcc5ec773243cf28d67
Patrick Perry <patperry@gmail.com>
2011-01-15 11:42:51 -0500

    Add logsumexp module

---
+++ src/logsumexp.c
@@ -0,0 +1,75 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <assert.h>
+#include <math.h>
+#include "logsumexp.h"
+
+
+/* Algorithm logsumexp
+ *   INPUT: a[1], ..., a[N]
+ *   OUTPUT: log(sum(exp(ai)))
+ *
+ *   INVARIANT: S[n] = sum(exp(a[i] - M[n])) - 1
+ *              M[n] = max(a[i])
+ *
+ *   S := -1
+ *   M := -INFINITY
+ *
+ *   for i = 1 to N do
+ *       if a[i] <= M
+ *       then
+ *           S := S + exp(a[i] - M)
+ *       else
+ *           S := (S + 1) * exp(M - a[i])
+ *           M := a[i]
+ *       end
+ *   end
+ *
+ *   return M + log(S + 1)
+ */
+void
+iproc_logsumexp_init (iproc_logsumexp *lse)
+{
+    assert(lse);
+    lse->max = -INFINITY;
+    lse->sumexpm1 = -1.0;
+}
+
+
+void
+iproc_logsumexp_insert (iproc_logsumexp *lse,
+                        double           val)
+{
+    assert(lse);
+
+    double sumexpm1 = lse->sumexpm1;
+    double max = lse->max;
+
+    if (!(val > max)) {
+        lse->sumexpm1 += exp(val - max);
+    } else {
+        double scale = exp(max - val);
+        lse->sumexpm1 = sumexpm1 * scale + scale;
+        lse->max = val;
+    }
+}
+
+double
+iproc_logsumexp_value (iproc_logsumexp *lse)
+{
+    assert(lse);
+
+    double sumexpm1 = lse->sumexpm1;
+    double max = lse->max;
+    
+    return max + log1p(sumexpm1);
+}
+
+double
+iproc_logsumexp_max (iproc_logsumexp *lse)
+{
+    assert(lse);
+    return lse->max;
+}
---
+++ src/logsumexp.h
@@ -0,0 +1,21 @@
+#ifndef _IPROC_LOGSUMEXP_H
+#define _IPROC_LOGSUMEXP_H
+
+typedef struct _iproc_logsumexp iproc_logsumexp;
+
+struct _iproc_logsumexp {
+    double max;
+    double sumexpm1;
+};
+
+void   iproc_logsumexp_init   (iproc_logsumexp *lse);
+
+void   iproc_logsumexp_insert (iproc_logsumexp *lse,
+                               double           value);
+double iproc_logsumexp_max    (iproc_logsumexp *lse);
+double iproc_logsumexp_value  (iproc_logsumexp *lse);
+
+
+
+
+#endif /* _IPROC_LOGSUMEXP_H */
