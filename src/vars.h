0de9a36b1323d8114070928b110d22c16ea17f34
Patrick Perry <patperry@gmail.com>
2011-01-18 16:52:52 -0500

    Whitespace

--- src/vars.h
+++ src/vars.h
@@ -37,51 +37,51 @@ struct _iproc_vars_ctx {
 };
 
 
-iproc_vars *     iproc_vars_new          (iproc_actors   *senders,
-                                          iproc_actors   *receivers);
-iproc_vars *     iproc_vars_ref          (iproc_vars     *vars);
-void             iproc_vars_unref        (iproc_vars     *vars);
-int64_t          iproc_vars_dim          (iproc_vars     *vars);
-int64_t          iproc_vars_nsender      (iproc_vars     *vars);
-int64_t          iproc_vars_nreceiver    (iproc_vars     *vars);
-iproc_actors *   iproc_vars_senders      (iproc_vars     *vars);
-iproc_actors *   iproc_vars_receivers    (iproc_vars     *vars);
+iproc_vars *     iproc_vars_new           (iproc_actors   *senders,
+                                           iproc_actors   *receivers);
+iproc_vars *     iproc_vars_ref           (iproc_vars     *vars);
+void             iproc_vars_unref         (iproc_vars     *vars);
+int64_t          iproc_vars_dim           (iproc_vars     *vars);
+int64_t          iproc_vars_nsender       (iproc_vars     *vars);
+int64_t          iproc_vars_nreceiver     (iproc_vars     *vars);
+iproc_actors *   iproc_vars_senders       (iproc_vars     *vars);
+iproc_actors *   iproc_vars_receivers     (iproc_vars     *vars);
 
-void             iproc_vars_sender0_mul  (double          alpha,
-                                          iproc_trans     trans,
-                                          iproc_vars     *vars,
-                                          int64_t         isend,
-                                          iproc_vector   *x,
-                                          double          beta,
-                                          iproc_vector   *y);
-void             iproc_vars_sender0_muls (double          alpha,
-                                          iproc_trans     trans,
-                                          iproc_vars     *vars,
-                                          int64_t         isend,
-                                          iproc_svector  *x,
-                                          double          beta,
-                                          iproc_vector   *y);
-
-
-iproc_vars_ctx * iproc_vars_ctx_new      (iproc_vars     *vars,
-                                          int64_t         isend,
-                                          iproc_history  *h);
-iproc_vars_ctx * iproc_vars_ctx_ref      (iproc_vars_ctx *ctx);
-void             iproc_vars_ctx_unref    (iproc_vars_ctx *ctx);
+void             iproc_vars_sender0_mul   (double          alpha,
+                                           iproc_trans     trans,
+                                           iproc_vars     *vars,
+                                           int64_t         isend,
+                                           iproc_vector   *x,
+                                           double          beta,
+                                           iproc_vector   *y);
+void             iproc_vars_sender0_muls  (double          alpha,
+                                           iproc_trans     trans,
+                                           iproc_vars     *vars,
+                                           int64_t         isend,
+                                           iproc_svector  *x,
+                                           double          beta,
+                                           iproc_vector   *y);
 
 
-void             iproc_vars_ctx_mul      (double          alpha,
-                                          iproc_trans     trans,
-                                          iproc_vars_ctx *ctx,
-                                          iproc_vector   *x,
-                                          double          beta,
-                                          iproc_vector   *y);
-void             iproc_vars_ctx_muls     (double          alpha,
-                                          iproc_trans     trans,
-                                          iproc_vars_ctx *ctx,
-                                          iproc_svector  *x,
-                                          double          beta,
-                                          iproc_vector   *y);
+iproc_vars_ctx * iproc_vars_ctx_new       (iproc_vars     *vars,
+                                           int64_t         isend,
+                                           iproc_history  *h);
+iproc_vars_ctx * iproc_vars_ctx_ref       (iproc_vars_ctx *ctx);
+void             iproc_vars_ctx_unref     (iproc_vars_ctx *ctx);
+
+
+void             iproc_vars_ctx_mul       (double          alpha,
+                                           iproc_trans     trans,
+                                           iproc_vars_ctx *ctx,
+                                           iproc_vector   *x,
+                                           double          beta,
+                                           iproc_vector   *y);
+void             iproc_vars_ctx_muls      (double          alpha,
+                                           iproc_trans     trans,
+                                           iproc_vars_ctx *ctx,
+                                           iproc_svector  *x,
+                                           double          beta,
+                                           iproc_vector   *y);
 
 void             iproc_vars_ctx_diff_mul  (double          alpha,
                                            iproc_trans     trans,
