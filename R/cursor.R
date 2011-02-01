ff0ec88a13ca481299965714e2f724ad4dc34035
Patrick Perry <patperry@gmail.com>
2011-02-01 17:15:08 -0500

    Add print method for cursor

--- R/cursor.R
+++ R/cursor.R
@@ -35,3 +35,27 @@ to.cursor <- function(cursor) {
     .Call("Riproc_cursor_to", cursor)
 }
 
+print.cursor <- function(x, ...) {
+    if (!started(x)) {
+        cat("(unstarted cursor)\n")
+    } else if (finished(x)) {
+        cat("(finished cursor)\n")
+    } else {
+        cat("cursor at time ", time(x), ":\n", sep='')
+        from <- from(x)
+        to <- to(x)
+
+        for (i in seq_along(from)) {
+            cat("    ", from[[i]], " -> {", sep='')
+            nto <- length(to[[i]])
+            if (nto > 0) {
+                cat(" ", to[[i]][1], sep='')
+            }
+            for (j in (1 + seq_len(nto - 1))) {
+                cat(", ", to[[i]][j], sep='')
+            }
+            cat(" }\n")
+        }
+    }
+    invisible(x)
+}
