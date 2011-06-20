8dce4c3658606f4503df3fae57fc8fdb93a1cd2f
Patrick Perry <patperry@gmail.com>
2011-06-20 15:34:37 -0400

    Add tests for recv_loglik score

--- tests/recv_loglik_test.c
+++ tests/recv_loglik_test.c
@@ -142,7 +142,7 @@ static void test_dev(void **state)
 }
 
 
-static void test_score(void **state)
+static void test_mean(void **state)
 {
 	struct recv_model *rm;
 	struct vector probs, mean0, mean1, avg_mean0, avg_mean1, diff;
@@ -179,7 +179,7 @@ static void test_score(void **state)
 			frame_add(&frame, msg);			
 			recv_loglik_add(&recv_loglik, &frame, msg);
 			n = recv_loglik_count(&recv_loglik);
-			if (n > 1000)
+			if (n > 5000)
 				goto out;
 
 			isend = msg->from;
@@ -224,6 +224,98 @@ out:
 	vector_deinit(&mean0);
 	vector_deinit(&mean1);	
 	vector_deinit(&probs);	
+}
+
+
+static void test_score(void **state)
+{
+	struct recv_model *rm;
+	struct vector score0, score1, avg_score0, avg_score1, diff;
+	struct svector nrecv;
+	struct messages_iter it;
+	const struct message *msg = NULL;
+	double t;
+	ssize_t isend, ito;
+	ssize_t itie, ntie;
+	ssize_t index, dim = design_recv_dim(&design);
+	ssize_t n;
+	
+	svector_init(&nrecv, design_recv_count(&design));
+	vector_init(&score0, design_recv_dim(&design));
+	vector_init(&score1, design_recv_dim(&design));
+	vector_init(&avg_score0, design_recv_dim(&design));
+	vector_init(&avg_score1, design_recv_dim(&design));
+	vector_init(&diff, design_recv_dim(&design));
+	
+	MESSAGES_FOREACH(it, &messages) {
+		t = MESSAGES_TIME(it);
+		
+		while (frame_next_change(&frame) <= t) {
+			model_update(&model, &frame);
+			frame_advance(&frame);			
+		}
+		if (frame_time(&frame) < t) {
+			model_update(&model, &frame);
+			frame_advance_to(&frame, t);			
+		}
+		
+		ntie = MESSAGES_COUNT(it);
+		for (itie = 0; itie < ntie; itie ++) {
+			msg = MESSAGES_VAL(it, itie);
+			frame_add(&frame, msg);			
+			recv_loglik_add(&recv_loglik, &frame, msg);
+			n = recv_loglik_count(&recv_loglik);
+			if (n > 5000)
+				goto out;
+			
+			isend = msg->from;
+			rm = model_recv_model(&model, isend);
+			
+			svector_clear(&nrecv);
+			for (ito = 0; ito < msg->nto; ito++) {
+				*svector_item_ptr(&nrecv, msg->to[ito]) += 1.0;
+			}
+
+			frame_recv_muls(1.0, TRANS_TRANS, &frame, isend, &nrecv,
+					0.0, &score0);
+			recv_loglik_axpy_last_mean(-1.0, &recv_loglik, &score0);
+			
+			vector_fill(&score1, 0.0);
+			recv_loglik_axpy_last_score(1.0, &recv_loglik, &score1);
+			
+			for (index = 0; index < dim; index++) {
+				double x0 = vector_item(&score0, index);
+				double x1 = vector_item(&score1, index);				
+				assert_in_range(double_eqrel(x0, x1), 40, DBL_MANT_DIG);
+			}
+			
+			vector_assign_copy(&diff, &avg_score0);
+			vector_axpy(-1.0/msg->nto, &score0, &diff);
+			vector_axpy(-((double)msg->nto) / n, &diff, &avg_score0);
+			
+			vector_fill(&avg_score1, 0.0);
+			recv_loglik_axpy_avg_score(1.0, &recv_loglik, &avg_score1);
+			
+			for (index = 0; index < dim; index++) {
+				double x0 = vector_item(&avg_score0, index);
+				double x1 = vector_item(&avg_score1, index);
+				if (fabs(x0) >= 5e-4) {
+					assert_in_range(double_eqrel(x0, x1), 37, DBL_MANT_DIG);
+				} else {
+					assert_true(fabs(x0 - x1) < sqrt(DBL_EPSILON));
+				}
+			}
+			
+			vector_assign_copy(&avg_score0, &avg_score1);
+		}
+	}
+out:
+	vector_deinit(&diff);
+	vector_deinit(&avg_score1);	
+	vector_deinit(&avg_score0);
+	vector_deinit(&score0);
+	vector_deinit(&score1);	
+	svector_deinit(&nrecv);	
 }
 
 
@@ -350,7 +442,8 @@ int main(int argc, char **argv)
 	UnitTest tests[] = {
 		unit_test_setup(enron_suite, enron_setup_fixture),
 		unit_test_setup_teardown(test_dev, basic_setup, basic_teardown),
-		unit_test_setup_teardown(test_score, basic_setup, basic_teardown),
+		unit_test_setup_teardown(test_mean, basic_setup, basic_teardown),
+		unit_test_setup_teardown(test_score, basic_setup, basic_teardown),		
 		unit_test_setup_teardown(test_imat, basic_setup, basic_teardown),
 		unit_test_teardown(enron_suite, enron_teardown_fixture),
 	};
