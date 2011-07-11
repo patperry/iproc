814850f0feb03abf3bae46bfcd15b7324534985c
Patrick Perry <patperry@gmail.com>
2011-07-11 15:45:45 -0700

    Simplify strata interface

--- src/strata.c
+++ src/strata.c
@@ -2,28 +2,6 @@
 #include <assert.h>
 #include "strata.h"
 
-static void stratum_init(struct stratum *s, const struct vector *traits)
-{
-	assert(s);
-	assert(traits);
-	vector_init_copy(&s->traits, traits);
-	intset_init(&s->ids);
-}
-
-
-static void stratum_deinit(struct stratum *s)
-{
-	assert(s);
-	intset_deinit(&s->ids);
-	vector_deinit(&s->traits);
-}
-
-
-static bool stratum_add(struct stratum *s, intptr_t id)
-{
-	assert(s);
-	return intset_add(&s->ids, id);
-}
 
 
 void strata_init(struct strata *s, ssize_t dim)
@@ -32,8 +10,8 @@ void strata_init(struct strata *s, ssize_t dim)
 	assert(dim >= 0);
 	
 	s->dim = dim;
-	array_init(&s->items, sizeof(struct stratum));
-	intmap_init(&s->trait_hashes, sizeof(struct intset),
+	array_init(&s->levels, sizeof(struct vector));
+	intmap_init(&s->level_hashes, sizeof(struct intset),
 		    alignof(struct intset));
 }
 
@@ -43,53 +21,72 @@ void strata_deinit(struct strata *s)
 	assert(s);
 	
 	struct intmap_iter it;
-	INTMAP_FOREACH(it, &s->trait_hashes) {
+	INTMAP_FOREACH(it, &s->level_hashes) {
 		struct intset *set = INTMAP_VAL(it);
 		intset_deinit(set);
 	}
-	intmap_deinit(&s->trait_hashes);
+	intmap_deinit(&s->level_hashes);
 	
-	struct stratum *item;
-	ARRAY_FOREACH(item, &s->items) {
-		stratum_deinit(item);
+	struct vector *level;
+	ARRAY_FOREACH(level, &s->levels) {
+		vector_deinit(level);
 	}
-	array_deinit(&s->items);
+	array_deinit(&s->levels);
 }
 
-
-ssize_t strata_add(struct strata *s, intptr_t id, const struct vector *traits)
+static ssize_t lookup_add(struct strata *s, const struct vector *level, bool add)
 {
 	assert(s);
-	assert(vector_dim(traits) == strata_dim(s));
-
-	struct stratum *item;
-	ssize_t index = -1;
-
-	int32_t hash = vector_hash(traits);
+	assert(level);
+	assert(vector_dim(level) == strata_dim(s));
+	
+	struct vector *stratum;
+	ssize_t index;
+	
+	int32_t hash = vector_hash(level);
 	struct intmap_pos pos;
-	struct intset *set = intmap_find(&s->trait_hashes, hash, &pos);
+	struct intset *set = intmap_find(&s->level_hashes, hash, &pos);
 	
 	if (!set) {
-		set = intmap_insert(&s->trait_hashes, &pos, NULL);
+		set = intmap_insert(&s->level_hashes, &pos, NULL);
 		intset_init(set);
 	}
 	
 	struct intset_iter it;
 	INTSET_FOREACH(it, set) {
 		index = INTSET_KEY(it);
-		item = array_item(&s->items, index);
-		const struct vector *x = stratum_traits(item);
-		if (vector_equals(x, traits)) {
+		stratum = array_item(&s->levels, index);
+		if (vector_equals(stratum, level)) {
 			goto found;
 		}
 	}
+	index = -1; /* level not found */
 	
-	/* cohort not found; create a new one */
-	index = array_count(&s->items);
-	item = array_add(&s->items, NULL);
-	stratum_init(item, traits);
-	intset_add(set, index);
+	/* level not found; create a new stratum */
+	if (add) {
+		index = array_count(&s->levels);
+		stratum = array_add(&s->levels, NULL);
+		vector_init_copy(stratum, level);
+		intset_add(set, index);
+	}
 found:
-	stratum_add(item, id);
 	return index;
+}
+
+ssize_t strata_add(struct strata *s, const struct vector *level)
+{
+	assert(s);
+	assert(level);	
+	assert(vector_dim(level) == strata_dim(s));
+
+	return lookup_add(s, level, true);
+}
+
+ssize_t strata_lookup(const struct strata *s, const struct vector *level)
+{
+	assert(s);
+	assert(level);	
+	assert(vector_dim(level) == strata_dim(s));
+	
+	return lookup_add((struct strata *)s, level, false);
 }
--- src/strata.h
+++ src/strata.h
@@ -7,15 +7,10 @@
 #include "vector.h"
 
 
-struct stratum {
-	struct vector traits;
-	struct intset ids;
-};
-
 struct strata {
 	ssize_t dim;
-	struct array items;
-	struct intmap trait_hashes;
+	struct array levels;
+	struct intmap level_hashes;
 };
 
 void strata_init(struct strata *s, ssize_t dim);
@@ -23,14 +18,10 @@ void strata_deinit(struct strata *s);
 
 static inline ssize_t strata_dim(const struct strata *s);
 static inline ssize_t strata_count(const struct strata *s);
-static inline struct stratum *strata_items(const struct strata *s);
 
-ssize_t strata_add(struct strata *s, intptr_t id, const struct vector *traits);
-
-static inline struct vector *stratum_traits(const struct stratum *s);
-static inline ssize_t stratum_count(const struct stratum *s);
-static inline intptr_t *stratum_items(const struct stratum *s);
-
+ssize_t strata_add(struct strata *s, const struct vector *level);
+ssize_t strata_lookup(const struct strata *s, const struct vector *level);
+static inline struct vector *strata_levels(const struct strata *s);
 
 /* inline function definitions */
 ssize_t strata_dim(const struct strata *s)
@@ -42,33 +33,14 @@ ssize_t strata_dim(const struct strata *s)
 ssize_t strata_count(const struct strata *s)
 {
 	assert(s);
-	return array_count(&s->items);
+	return array_count(&s->levels);
 }
 
-struct stratum *strata_items(const struct strata *s)
+struct vector *strata_levels(const struct strata *s)
 {
 	assert(s);
-	return array_to_ptr(&s->items);
+	return array_to_ptr(&s->levels);
 }
-
-struct vector *stratum_traits(const struct stratum *s)
-{
-	assert(s);
-	return &((struct stratum *)s)->traits;
-}
-
-ssize_t stratum_count(const struct stratum *s)
-{
-	assert(s);
-	return intset_count(&s->ids);
-}
-
-intptr_t *stratum_items(const struct stratum *s)
-{
-	assert(s);
-	return intset_items(&s->ids);
-}
-
 
 
 #endif /* _STRATA_H */
