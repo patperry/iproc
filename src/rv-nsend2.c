f83d79661b7f07c5f534e9b631c8eaafbfadb369
Patrick Perry <patperry@gmail.com>
2011-08-12 08:48:11 -0400

    Bugfixes

--- src/rv-nsend2.c
+++ src/rv-nsend2.c
@@ -29,16 +29,45 @@ static void handle_message_add(void *udata, struct frame *f,
 	assert(fv->design->dyn_index + fv->design->dim
 	       <= design_recv_dyn_dim(f->design));
 
+	const struct design *d = frame_design(f);
+	const struct vector *intvls = design_intervals(d);
+	ssize_t nintvl = vector_dim(intvls);
 	ssize_t isend = msg->from;
 	ssize_t dyn_index = fv->design->dyn_index;
-
+	ssize_t *imsg, i, n;	
+	
 	ssize_t ito, nto = msg->nto;
 	for (ito = 0; ito < nto; ito++) {
 		ssize_t jrecv = msg->to[ito];
 
-		ssize_t i, n = fv->design->dim;
+		frame_get_send_messages(f, jrecv, &imsg, &n);
 		for (i = 0; i < n; i++) {
-			frame_recv_update(f, isend, jrecv, dyn_index + i, 1.0);
+			const struct frame_message *fmsg = frame_messages_item(f, imsg[i]);
+			const struct message *msg1 = fmsg->message;
+			ssize_t intvl1 = fmsg->interval;
+			ssize_t ix = dyn_index + intvl1 * (nintvl + 1);
+			assert(msg1->from == jrecv);
+			
+			ssize_t ito1, nto1 = msg1->nto;
+			for (ito1 = 0; ito1 < nto1; ito1++) {
+				ssize_t jrecv1 = msg1->to[ito1];
+				
+				frame_recv_update(f, isend, jrecv1, ix, +1.0);								
+			}
+		}
+	}
+		
+	frame_get_recv_messages(f, isend, &imsg, &n);
+	for (i = 0; i < n; i++) {
+		const struct frame_message *fmsg = frame_messages_item(f, imsg[i]);
+		const struct message *msg1 = fmsg->message;
+		ssize_t intvl1 = fmsg->interval;
+		ssize_t ix = dyn_index + intvl1;
+		ssize_t isend1 = msg1->from;
+		
+		for (ito = 0; ito < nto; ito++) {
+			ssize_t jrecv = msg->to[ito];
+			frame_recv_update(f, isend1, jrecv, ix, +1.0);
 		}
 	}
 }
@@ -63,35 +92,44 @@ static void handle_message_advance(void *udata, struct frame *f,
 	ssize_t dyn_index = fv->design->dyn_index;
 
 	ssize_t ito, nto = msg->nto;
+	ssize_t *imsg, i, n;
+
 	for (ito = 0; ito < nto; ito++) {
 		ssize_t jrecv = msg->to[ito];
-		ssize_t *imsg, i, n;
+
 		
 		frame_get_send_messages(f, jrecv, &imsg, &n);
 		for (i = 0; i < n; i++) {
 			const struct frame_message *fmsg = frame_messages_item(f, imsg[i]);
 			const struct message *msg1 = fmsg->message;
 			ssize_t intvl1 = fmsg->interval;
-			ssize_t ix = dyn_index + (intvl - 1) + intvl1 * (nintvl + 1);
+			ssize_t ix0 = dyn_index + (intvl - 1) + intvl1 * (nintvl + 1);
+			ssize_t ix1 = ix0 + 1;
 			assert(msg1->from == jrecv);
 			
 			ssize_t ito1, nto1 = msg1->nto;
 			for (ito1 = 0; ito1 < nto1; ito1++) {
 				ssize_t jrecv1 = msg1->to[ito1];
-				
-				frame_recv_update(f, isend, jrecv1, ix, -1.0);
+
+				frame_recv_update(f, isend, jrecv1, ix1, +1.0);								
+				frame_recv_update(f, isend, jrecv1, ix0, -1.0);
 			}
 		}
+	}
 
-		frame_get_recv_messages(f, isend, &imsg, &n);
-		for (i = 0; i < n; i++) {
-			const struct frame_message *fmsg = frame_messages_item(f, imsg[i]);
-			const struct message *msg1 = fmsg->message;
-			ssize_t intvl1 = fmsg->interval;
-			ssize_t ix = dyn_index + intvl1 + (intvl - 1) * (nintvl + 1);
-			
-			ssize_t isend1 = msg1->from;
-			frame_recv_update(f, isend1, jrecv, ix, -1.0);
+	frame_get_recv_messages(f, isend, &imsg, &n);
+	for (i = 0; i < n; i++) {
+		const struct frame_message *fmsg = frame_messages_item(f, imsg[i]);
+		const struct message *msg1 = fmsg->message;
+		ssize_t intvl1 = fmsg->interval;
+		ssize_t ix0 = dyn_index + intvl1 + (intvl - 1) * (nintvl + 1);
+		ssize_t ix1 = ix0 + (nintvl + 1);
+		ssize_t isend1 = msg1->from;
+
+		for (ito = 0; ito < nto; ito++) {
+			ssize_t jrecv = msg->to[ito];
+			frame_recv_update(f, isend1, jrecv, ix1, +1.0);			
+			frame_recv_update(f, isend1, jrecv, ix0, -1.0);
 		}
 	}
 }
