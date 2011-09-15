2125e765395661749f4066e3e7ff49976f6ff310
Patrick Perry <patperry@gmail.com>
2011-09-15 17:33:58 -0400

    Update process.R for sparse constraints; add bootprocess script

---
+++ tools/bootprocess.R
@@ -0,0 +1,88 @@
+# tools/bootprocess.R
+# -------------------
+
+source("tools/process.R")
+
+fit0 <- fromJSON("output/fit-dynamic.json")
+coefs0 <- get.coefs(fit0)
+
+nreps <- length(list.files("output/boot-dynamic", "*.json"))
+coefs <- array(NA, c(dim(coefs0), nreps))
+dimnames(coefs) <- c(dimnames(coefs0), NULL)
+
+for (r in seq_len(nreps)) {
+    fn <- paste("output/boot-dynamic/fit", r, ".json", sep='')
+    fit <- fromJSON(fn)
+    coefs[,,r] <- get.coefs(fit)
+    cat('.')
+}
+
+tx <- get.transform()
+tcoefs0 <- coefs0 %*% tx
+
+tcoefs <- array(NA, dim(coefs))
+for (r in seq_len(nreps)) {
+    tcoefs[,,r] <- coefs[,,r] %*% tx
+}
+
+tix0 <- rep(1:9, 10) + rep((1:10 - 1) * nrow(tcoefs0), each=9)
+tix1 <- 12:nrow(tcoefs0)
+tix <- c(tix0, tix1)
+
+bias <- array(NA, dim(tcoefs))
+for (r in seq_len(nreps)) {
+    bias[,,r] <- tcoefs[,,r] - tcoefs0
+}
+
+cov <- get.cov(fit0)
+vtx <- get.transform(TRUE)
+
+ix0 <- rep(1:11, 12) + rep((1:12 - 1) * nrow(coefs0), each=11)
+
+cov0 <- cov[ix0, ix0]
+tcov0 <- vtx %*% cov0 %*% t(vtx)
+cov1 <- cov[12:(nrow(cov)/12), 12:(nrow(cov)/12)]
+se0 <- as.vector(matrix(sqrt(pmax(0, diag(tcov0))), 11, 12)[1:9, 1:10])
+se1 <- sqrt(pmax(0, diag(cov1)))
+se <- c(se0, se1)
+
+bias.mean <- apply(bias, c(1,2), mean)[tix]
+bias.sd <- apply(bias, c(1,2), sd)[tix]
+
+save(bias.mean, bias.sd, se, bias, "boot.rda")
+
+
+par(las = 1)
+
+ix <- seq_along(bias.mean)
+
+color.segments <- 2
+color.points <- 1
+
+plot(range(ix),
+     c(min((bias.mean - bias.sd) / se),
+       max((bias.mean + bias.sd) / se)), 
+     t = "n", axes = FALSE,
+     xlab = "Coefficient Index",
+     ylab = "Normalized Residual") 
+#abline(v = nstatic + 1, lty = 2, col = "gray")
+#xaxis.at <- c(1, 73, 145)
+#axis(1, at = xaxis.at, labels = TRUE)
+#axis(3, at = xaxis.at, labels = FALSE)
+axis(2, labels = TRUE)
+axis(4, labels = FALSE)
+box()
+
+segments(ix, ((bias.mean - bias.sd) / se)[ix],
+         ix, ((bias.mean + bias.sd) / se)[ix],
+         col = color.segments)
+segments(ix - 0.45, ((bias.mean - bias.sd) / se)[ix],
+         ix + 0.45, ((bias.mean - bias.sd) / se)[ix],
+         col = color.segments)
+segments(ix - 0.45, ((bias.mean + bias.sd) / se)[ix],
+         ix + 0.45, ((bias.mean + bias.sd) / se)[ix],
+         col = color.segments)
+
+points(ix, (bias.mean / se)[ix],
+       pch = 16, cex = 0.5, col = color.points)
+
--- tools/process.R
+++ tools/process.R
@@ -1,5 +1,8 @@
 require(RJSONIO)
+require(Matrix)
+
 jmat <- function(j) { matrix(j$data, j$nrow, j$ncol) }
+jsvec <- function(j) { spMatrix(j$dim, 1, j$pattern, rep(1L, j$count), j$data) }
 
 #fit <- fromJSON("~/Projects/iproc/fit.json")
 
@@ -50,80 +53,95 @@ get.duals <- function(fit) {
     duals
 }
 
+get.constraints <- function(fit) {
+    nce <- length(fit$constraints)
+    dim <- fit$coefficients$nrow * fit$coefficients$ncol
+    nnz <- lapply(fit$constraints, function(x) x$count)
+    i <- rep(1:nce, nnz)
+    j <- c(lapply(fit$constraints, function(x) x$pattern), recursive=TRUE)
+    x <- c(lapply(fit$constraints, function(x) x$data), recursive=TRUE)
+    a <- sparseMatrix(i, j, x = x, dims = c(nce, dim))
+    b <- fit$constraint_values
+    list(matrix = a, values = b)
+}
+
 get.imat <- function(fit, duals = FALSE) {
-        n <- fit$count
-        ntot <- sum(n)
-        imatc <- lapply(fit$information, jmat)
-        ce <- jmat(fit$constraints)
-        dim <- nrow(imatc[[1]])
-        nc <- length(imatc)
-        ne <- ncol(ce)
+    dim <- fit$coefficients$nrow
+    nc <- fit$coefficients$ncol
+    nce <- length(fit$constraints)
 
-        m <- dim*nc
-        if (duals)
-            m <- m + ne
+    n <- fit$count
+    ntot <- sum(n)
+    imatc <- lapply(fit$information, jmat)
 
-        imat <- matrix(0, m, m)
-        for (ic in 1:nc) {
-            off <- dim * (ic - 1)
-            imat[off + 1:dim, off + 1:dim] <- (n[[ic]]/ntot) * imatc[[ic]]
-        }
-        imat
+
+    m <- dim*nc
+    if (duals)
+        m <- m + nce
+
+    imat <- sparseMatrix(integer(), integer(), x = numeric(), dims = c(m, m))
+    for (ic in 1:nc) {
+        off <- dim * (ic - 1)
+        imat[off + 1:dim, off + 1:dim] <- (n[[ic]]/ntot) * imatc[[ic]]
+    }
+    imat
 }
 
 get.kkt <- function(fit) {
-        n <- fit$count
-        ntot <- sum(n)
-        ce <- jmat(fit$constraints)
+    dim <- fit$coefficients$nrow
+    nc <- fit$coefficients$ncol
+    nce <- length(fit$constraints)
 
-        imatc <- lapply(fit$information, jmat)
-        dim <- nrow(imatc[[1]])
-        nc <- length(imatc)
-        ne <- ncol(ce)
+    n <- fit$count
+    ntot <- sum(n)
+    ce <- get.constraints(fit)$matrix
 
-        m <- dim * nc + ne
-        kkt <- matrix(0, m, m)
-        kkt[1:(dim*nc), 1:(dim*nc)] <- get.imat(fit, duals=FALSE)
-        if (ne > 0) {
-                kkt[1:(dim*nc), dim * nc + 1:ne] <- ce
-                kkt[dim * nc + 1:ne, 1:(dim*nc)] <- t(ce)
-        }
-        kkt
+    m <- dim * nc + nce
+    kkt <- get.imat(fit, duals=TRUE)
+    if (nce > 0) {
+        kkt[1:(dim*nc), dim * nc + 1:nce] <- t(ce)
+        kkt[dim * nc + 1:nce, 1:(dim*nc)] <- ce
+    }
+    kkt
 }
 
 get.cov <- function(fit, duals=FALSE) {
-        kkt <- get.kkt(fit)
-        imat <- get.imat(fit, duals=TRUE)
-        n <- sum(fit$count)
+    dim <- fit$coefficients$nrow
+    nc <- fit$coefficients$ncol
 
-	s <- diag(kkt)
-	s[s < 1e-8] <- 1.0
-	s <- sqrt(s)
+    kkt <- get.kkt(fit)
+    imat <- get.imat(fit, duals=TRUE)
+    n <- sum(fit$count)
 
-        skkt <- sweep(kkt / s, 2, s, "/")
-	simat <- sweep(imat / s, 2, s, "/")
+    s <- diag(kkt)
+    s[s < 1e-8] <- 1.0
+    s <- sqrt(s)
 
-        qr.skkt <- qr(skkt)
-        scov <- qr.solve(qr.skkt, t(qr.solve(qr.skkt, simat / n)))
-        #scov <- solve(skkt, t(solve(skkt, simat / n)))
-        cov <- sweep(scov / s, 2, s, "/")
-	cov
+    skkt <- t(t(kkt) / s) / s
+    simat <- t(t(imat) / s) / s
+    skkti <- solve(skkt)
+    scov <- skkti %*% (simat / n) %*% skkti
+    cov <- t(t(scov) / s) / s
+
+    if (!duals) {
+        m <- dim * nc
+        cov <- cov[seq_len(m), seq_len(m), drop = FALSE]
+    }
+    cov
 }
 
 get.se <- function(fit) {
-       imatc <- lapply(fit$information, jmat)
-       ce <- jmat(fit$constraints)
-       dim <- nrow(imatc[[1]])
-       nc <- length(imatc)
-       ne <- ncol(ce)
+    dim <- fit$coefficients$nrow
+    nc <- fit$coefficients$ncol
+    nce <- length(fit$constraints)
 
-       cov <- get.cov(fit, duals = TRUE)
-       se2 <- diag(cov)
-       se2[se2 < 0] <- 0
-       se <- sqrt(se2)
+    cov <- get.cov(fit, duals = TRUE)
+    se2 <- diag(cov)
+    se2[se2 < 0] <- 0
+    se <- sqrt(se2)
 
-       se.coefs <- matrix(se[1:(dim*nc)], dim, nc)
-       se.duals <- se[dim*nc + 1:ne]
-       list(coefs = se.coefs, duals = se.duals)
+    se.coefs <- matrix(se[seq_len(dim*nc)], dim, nc)
+    se.duals <- se[dim*nc + seq_len(nce)]
+    list(coefs = se.coefs, duals = se.duals)
 }
 
