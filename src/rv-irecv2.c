6c4c08f607b702b530347fe5e266955f4823c5b3
Patrick Perry <patperry@gmail.com>
2011-10-24 21:40:50 -0400

    Switch to faster irecv2

--- src/rv-irecv2.c
+++ src/rv-irecv2.c
@@ -33,82 +33,57 @@ static void irecv2_message_add(void *udata, struct frame *f,
 	assert(msg);
 	assert(v->dyn_index + v->dim <= design_dvars_dim(frame_recv_design(f)));
 
+	size_t ito, nto = msg->nto;
 	size_t dyn_index = v->dyn_index;
 	double dx_data[1] = { 1.0 };
 	size_t dx_index[1] = { dyn_index };
 	struct vpattern pat = vpattern_make(dx_index, 1);
-	size_t *imsg, i, n;
-
-	size_t krecv = msg->from;
-	size_t cojrecv = msg->from;
-
-	size_t ito, nto = msg->nto;
-
-	frame_get_recv_messages(f, krecv, &imsg, &n);
-	for (i = 0; i < n; i++) {
-		const struct frame_message *fmsg =
-		    frame_messages_item(f, imsg[i]);
-		const struct message *msg1 = fmsg->message;
-
-		if (msg1->from == msg->from)
-			continue;
-
-		size_t jrecv = msg1->from;
-
+	
+	const struct frame_actor *fa;
+	size_t iz, nz;
+	
+	size_t hrecv = msg->from;
+	fa = &f->receivers[hrecv];
+	nz = fa->active.nz;
+	
+	for (iz = 0; iz < nz; iz++) {
+		size_t jrecv = fa->active.indx[iz];
+		
 		for (ito = 0; ito < nto; ito++) {
-			if (msg->to[ito] == msg->from
-			    || msg->to[ito] == msg1->from)
-				continue;
-
 			size_t isend = msg->to[ito];
-
-			assert(isend != jrecv);
-			assert(isend != krecv);
-			assert(jrecv != krecv);
-
-			const struct vector *dx =
-			    frame_recv_dx(f, isend, jrecv);
-
+				
+			if (hrecv == isend || hrecv == jrecv || isend == jrecv)
+				continue;
+			
+			const struct vector *dx = frame_recv_dx(f, isend, jrecv);
+			
 			if (vector_item(dx, dyn_index) == 0.0) {
 				frame_recv_update(f, isend, jrecv, dx_data, &pat);
 			}
 		}
 	}
-
+	
+	size_t cojrecv = msg->from;
+	
 	for (ito = 0; ito < nto; ito++) {
-		if (msg->from == msg->to[ito])
-			continue;
-
-		size_t coksend = msg->to[ito];
-
-		frame_get_send_messages(f, coksend, &imsg, &n);
-		for (i = 0; i < n; i++) {
-			const struct frame_message *fmsg =
-			    frame_messages_item(f, imsg[i]);
-			const struct message *msg1 = fmsg->message;
-
-			size_t ito1, nto1 = msg1->nto;
-			for (ito1 = 0; ito1 < nto1; ito1++) {
-				if (msg1->to[ito1] == msg1->from
-				    || msg1->to[ito1] == msg->from)
-					continue;
-
-				size_t coisend = msg1->to[ito1];
-
-				assert(coisend != cojrecv);
-				assert(coisend != coksend);
-				assert(cojrecv != coksend);
-
-				const struct vector *dx =
-				    frame_recv_dx(f, coisend, cojrecv);
-
-				if (vector_item(dx, dyn_index) == 0.0) {
-					frame_recv_update(f, coisend, cojrecv,
-							  dx_data, &pat);
-				}
+		size_t cohsend = msg->to[ito];
+		fa = &f->senders[cohsend];
+		nz = fa->active.nz;
+		
+		for (iz = 0; iz < nz; iz++) {
+			size_t coisend = fa->active.indx[iz];
+				
+			if (cohsend == coisend || cohsend == cojrecv || coisend == cojrecv)
+				continue;
+			
+			const struct vector *dx = frame_recv_dx(f, coisend, cojrecv);
+			
+			if (vector_item(dx, dyn_index) == 0.0) {
+				frame_recv_update(f, coisend, cojrecv, dx_data, &pat);
 			}
 		}
 	}
+
 }
 
 static struct var_type RECV_VAR_IRECV2_REP = {
