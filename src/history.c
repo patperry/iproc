080533f0e5a8e8a10edd9140ad65e511b6e67838
Patrick Perry <patperry@gmail.com>
2013-05-29 01:38:00 -0400

    Start massive rewrite; add new history module

--- src/history.c
+++ src/history.c
@@ -1,490 +1,230 @@
 #include "port.h"
-#include <assert.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 #include "coreutil.h"
 #include "xalloc.h"
-#include "ieee754.h"
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #include "history.h"
 
 
-static void grow_ind_array(struct history_actor *a, size_t nintvl, size_t delta);
-static size_t search_ind(struct history_actor *a, size_t nintvl, size_t i);
-static size_t find_ind(const struct history_actor *a, size_t i);
-
-
-void grow_ind_array(struct history_actor *a, size_t nintvl, size_t delta)
+static void actors_init(struct history_actor **has, size_t n)
 {
-	size_t nz = a->nz;
-	size_t nz1 = nz + delta;
-
-	if (needs_grow(nz1, &a->nzmax)) {
-		a->ind = xrealloc(a->ind, a->nzmax * sizeof(a->ind[0]));
-		a->nmsg = xrealloc(a->nmsg, a->nzmax * nintvl * sizeof(a->nmsg[0]));
-	}
-}
-
-
-size_t search_ind(struct history_actor *a, size_t nintvl, size_t i)
-{
-	const size_t *base = a->ind, *ptr;
-	size_t nz;
-
-	for (nz = a->nz; nz != 0; nz >>= 1) {
-		ptr = base + (nz >> 1);
-		if (i == *ptr) {
-			return ptr - a->ind;
-		}
-		if (i > *ptr) {
-			base = ptr + 1;
-			nz--;
-		}
-	}
-
-	/* not found */
-	size_t iz = base - a->ind;
-	size_t ntail = a->nz - iz;
-
-	grow_ind_array(a, nintvl, 1);
-	memmove(a->ind + iz + 1, a->ind + iz, ntail * sizeof(a->ind[0]));
-	memmove(a->nmsg + (iz + 1) * nintvl, a->nmsg + iz * nintvl,
-		ntail * nintvl * sizeof(a->nmsg[0]));
-
-	a->ind[iz] = i;
-	memset(a->nmsg + iz * nintvl, 0, nintvl * sizeof(a->nmsg[0]));
-	a->nz++;
-
-	return iz;
-}
-
-size_t find_ind(const struct history_actor *a, size_t i)
-{
-	const size_t *base = a->ind, *ptr;
-	size_t nz;
-
-	for (nz = a->nz; nz != 0; nz >>= 1) {
-		ptr = base + (nz >> 1);
-		if (i == *ptr) {
-			return ptr - a->ind;
-		}
-		if (i > *ptr) {
-			base = ptr + 1;
-			nz--;
-		}
-	}
-
-	/* not found */
-	return a->nz;
-}
-
-
-static void history_actor_init(struct history_actor *actr)
-{
-	actr->message_ixs = NULL;
-	actr->nix = 0;
-	actr->nix_max = 0;
-	actr->nmsg = NULL;
-	actr->ind = NULL;
-	actr->nz = 0;
-	actr->nzmax = 0;
-}
-
-static void history_actor_clear(struct history_actor *actr)
-{
-	actr->nix = 0;
-	actr->nz = 0;
-}
-
-static void history_actor_grow_ixs(struct history_actor *actr, size_t delta)
-{
-	if (needs_grow(actr->nix + delta, &actr->nix_max)) {
-		actr->message_ixs = xrealloc(actr->message_ixs, actr->nix_max * sizeof(actr->message_ixs[0]));
-	}
-}
-
-static void history_actor_deinit(struct history_actor *actr)
-{
-	assert(actr);
-	free(actr->nmsg);
-	free(actr->ind);
-	free(actr->message_ixs);
-}
-
-
-static void history_actors_init(struct history_actor **actrsp, size_t n)
-{
-	assert(actrsp);
-
-	struct history_actor *actrs = xcalloc(n, sizeof(actrs[0]));
+	struct history_actor *ha;
 	size_t i;
-	for (i = 0; i < n; i++) {
-		history_actor_init(&actrs[i]);
-	}
 
-	*actrsp = actrs;
+	*has = xmalloc(n * sizeof(struct history_actor));
+	for (i = 0; i < n; i++) {
+		ha = &(*has)[i];
+		uintset_init(&ha->msgs);
+		uintset_init(&ha->alters);
+		ha->wts = NULL;
+	}
 }
 
-static void history_actors_clear(struct history_actor *actrs, size_t n)
+
+static void actors_deinit(struct history_actor *has, size_t n)
 {
+	struct history_actor *ha;
 	size_t i;
+
 	for (i = 0; i < n; i++) {
-		history_actor_clear(&actrs[i]);
+		ha = &has[i];
+		free(ha->wts);
+		uintset_deinit(&ha->alters);
+		uintset_deinit(&ha->msgs);
 	}
+	free(has);
 }
 
-static void history_actors_deinit(struct history_actor *actrs, size_t n)
+
+static void actors_clear(struct history_actor *has, size_t n)
 {
+	struct history_actor *ha;
 	size_t i;
+
 	for (i = 0; i < n; i++) {
-		history_actor_deinit(&actrs[i]);
+		ha = &has[i];
+		uintset_clear(&ha->msgs);
+		uintset_clear(&ha->alters);
 	}
-
-	free(actrs);
 }
 
-static int history_event_rcompare(const struct pqueue *q, const void *x1,
-				const void *x2)
+
+static void actor_add_msg(struct history_actor *ha, size_t i)
 {
-	(void)q;
-	const struct history_event *e1 = x1;
-	const struct history_event *e2 = x2;
-	return double_rcompare(&e1->time, &e2->time);
+	size_t n = uintset_count(&ha->msgs);
+
+	/* we always add messages in increasing order of index */
+	assert(n == 0 || ha->msgs.vals[n - 1] < i);
+
+	uintset_insert(&ha->msgs, n, i);
 }
 
-void history_init(struct history *h, size_t nsend, size_t nrecv, const double *intvls, size_t nintvl)
-{
-	assert(h);
-	assert(intvls || !nintvl);
-#ifndef NDEBUG
-	{
-		if (nintvl)
-			assert(0 < intvls[0]);
 
-		size_t i;
-		for (i = 1; i < nintvl; i++) {
-			assert(intvls[i - 1] < intvls[i]);
+static void actor_add_alter(struct history_actor *ha, size_t j, double wt)
+{
+	size_t index;
+
+	assert(!isnan(wt));
+
+	/* find the alter index */
+	if (!uintset_find(&ha->alters, j, &index)) {
+		size_t n0, n1, ntail;
+
+		/* insert the new alter  */
+		n0 = uintset_capacity(&ha->alters);
+		ntail = uintset_insert(&ha->alters, index, j);
+		n1 = uintset_capacity(&ha->alters);
+
+		/* expand the weights array if necessary */
+		if (n1 != n0) {
+			ha->wts = xrealloc(ha->wts, n1 * sizeof(double));
 		}
-	}
-#endif
 
+		/* initialize the new weight to zero */
+		memmove(ha->wts + index + 1, ha->wts + index,
+			ntail * sizeof(double));
+		ha->wts[index] = 0;
+	}
+
+	/* increment the alter weight */
+	ha->wts[index] += wt;
+}
+
+
+void history_init(struct history *h, size_t nsend, size_t nrecv)
+{
 	h->nsend = nsend;
 	h->nrecv = nrecv;
-	h->intvls = xmemdup(intvls, nintvl * sizeof(intvls[0]));
-	h->nintvl = nintvl;
-
-	h->observers = NULL;
-	h->nobs = 0;
-	h->nobs_max = 0;
-
 	h->msgs = NULL;
 	h->nmsg = 0;
 	h->nmsg_max = 0;
-
-	history_actors_init(&h->senders, nsend);
-	history_actors_init(&h->receivers, nrecv);
-	h->cur_msg = 0;
-	pqueue_init(&h->events, sizeof(struct history_event),
-		    history_event_rcompare);
-	history_clear(h);
+	h->to = NULL;
+	h->nto = 0;
+	h->nto_max = 0;
+	actors_init(&h->sends, nsend);
+	actors_init(&h->recvs, nrecv);
+	h->ncur = 0;
+	h->tcur = -INFINITY;
 }
+
 
 void history_deinit(struct history *h)
 {
-	assert(h);
-
-	size_t nsend = history_send_count(h);
-	size_t nrecv = history_recv_count(h);
-
-	pqueue_deinit(&h->events);
-	history_actors_deinit(h->receivers, nrecv);
-	history_actors_deinit(h->senders, nsend);
+	actors_deinit(h->recvs, history_nrecv(h));
+	actors_deinit(h->sends, history_nsend(h));
+	free(h->to);
 	free(h->msgs);
-	free(h->observers);
-	free(h->intvls);
 }
+
 
 void history_clear(struct history *h)
 {
-	assert(h);
-
-	size_t nsend = history_send_count(h);
-	size_t nrecv = history_recv_count(h);
-
-	h->time = -INFINITY;
 	h->nmsg = 0;
-	history_actors_clear(h->senders, nsend);
-	history_actors_clear(h->receivers, nrecv);
-	h->cur_msg = 0;
-	pqueue_clear(&h->events);
-
-	size_t i, n = h->nobs;
-	const struct history_observer *obs;
-	for (i = 0; i < n; i++) {
-		obs = &h->observers[i];
-		if (obs->callbacks.clear) {
-			obs->callbacks.clear(obs->udata, h);
-		}
-	}
+	h->nto = 0;
+	history_reset(h);
 }
 
-static void history_observers_grow(struct history *h, size_t delta)
+
+void history_reset(struct history *h)
 {
-	if (needs_grow(h->nobs + delta, &h->nobs_max)) {
-		h->observers = xrealloc(h->observers, h->nobs_max * sizeof(h->observers[0]));
-	}
+	actors_clear(h->sends, history_nsend(h));
+	actors_clear(h->recvs, history_nrecv(h));
+	h->ncur = 0;
+	h->tcur = -INFINITY;
 }
 
-void history_add_observer(struct history *h, void *udata,
-			const struct history_callbacks *callbacks)
-{
-	assert(h);
-	assert(udata);
-	assert(callbacks);
-
-	history_observers_grow(h, 1);
-	struct history_observer *obs = &h->observers[h->nobs++];
-	obs->udata = udata;
-	obs->callbacks = *callbacks;
-}
-
-void history_remove_observer(struct history *h, void *udata)
-{
-	assert(h);
-	assert(udata);
-
-	size_t i, n = h->nobs;
-	for (i = n; i > 0; i--) {
-		if (h->observers[i-1].udata == udata) {
-			memmove(h->observers + i - 1, h->observers + i,
-				(n - i) * sizeof(h->observers[0]));
-			h->nobs = n - 1;
-			break;
-		}
-	}
-}
-
-static void history_messages_grow(struct history *h, size_t delta)
-{
-	if (needs_grow(h->nmsg + delta, &h->nmsg_max)) {
-		h->msgs = xrealloc(h->msgs, h->nmsg_max * sizeof(h->msgs[0]));
-	}
-}
-
-void history_add(struct history *h, const struct message *msg)
-{
-	assert(h);
-	assert(msg);
-	assert(msg->time == history_time(h));
-
-	//fprintf(stderr, "================= Add (%p) =================\n", msg);
-
-	history_messages_grow(h, 1);
-	struct history_message *hmsg = &h->msgs[h->nmsg++];
-	hmsg->message = msg;
-	hmsg->interval = 0;
-}
-
-static int has_pending_messages(const struct history *h)
-{
-	assert(h->cur_msg <= h->nmsg);
-	return h->cur_msg < h->nmsg;
-}
-
-static void process_current_messages(struct history *h)
-{
-	assert(h);
-
-	if (!has_pending_messages(h))
-		return;
-
-	const double *intvls = history_intervals(h);
-	size_t nintvl = history_interval_count(h);
-	double delta = nintvl ? intvls[0] : INFINITY;
-	double tnext = history_time(h) + delta;
-
-	const struct history_message *hmsgs = h->msgs;
-	size_t imsg, nmsg = h->nmsg;
-	for (imsg = h->cur_msg; imsg < nmsg; imsg++) {
-		const struct history_message *hmsg = &hmsgs[imsg];
-		const struct message *msg = hmsg->message;
-		assert(msg->time == history_time(h));
-
-		// add the message to the sender history
-		struct history_actor *actr;
-		size_t ito, nto = msg->nto;
-
-		actr = &h->senders[msg->from];
-		history_actor_grow_ixs(actr, 1);
-		actr->message_ixs[actr->nix++] = imsg;
-
-		for (ito = 0; ito < nto; ito++) {
-			size_t ix = search_ind(actr, nintvl, msg->to[ito]);
-			size_t *ptr = actr->nmsg + ix * nintvl;
-			if (nintvl)
-				ptr[0]++;
-		}
-
-		// add the message to the receiver histories
-		for (ito = 0; ito < nto; ito++) {
-			actr = &h->receivers[msg->to[ito]];
-			history_actor_grow_ixs(actr, 1);
-			actr->message_ixs[actr->nix++] = imsg;
-			size_t ix = search_ind(actr, nintvl, msg->from);
-			size_t *ptr = actr->nmsg + ix * nintvl;
-			if (nintvl)
-				ptr[0]++;
-		}
-
-		// create an advance event (if necessary)
-		if (isfinite(tnext)) {
-			struct history_event e;
-			e.time = tnext;
-			e.imsg = imsg;
-			pqueue_push(&h->events, &e);
-		}
-#ifndef NDEBUG
-		size_t nmsg1 = h->nmsg;
-		double time = history_time(h);
-#endif
-
-		//fprintf(stderr, "-> message_add %p, { %zu -> [ ", msg, msg->from);
-
-		//if (msg->nto > 0)
-		//      fprintf(stderr, "%zu", msg->to[0]);
-		//for (ito = 1; ito < msg->nto; ito++) {
-		//      fprintf(stderr, ", %zu", msg->to[ito]);
-		//}
-		//fprintf(stderr, " ] }\n");
-
-		// notify all observers
-		size_t i, n = h->nobs;
-		const struct history_observer *obs;
-		for (i = 0; i < n; i++) {
-			obs = &h->observers[i];
-			if (obs->callbacks.message_add) {
-				obs->callbacks.message_add(obs->udata, h, msg);
-
-				// make sure observers don't add messages
-				// or advance time
-				assert(nmsg == nmsg1);
-				assert(history_time(h) == time);
-			}
-		}
-	}
-	h->cur_msg = nmsg;
-}
-
-static void process_event(struct history *h)
-{
-	assert(pqueue_count(&h->events));
-	assert(!has_pending_messages(h));
-	assert(history_time(h) == history_next_time(h));
-
-	struct history_event *e = pqueue_top(&h->events);
-
-	// advance the message interval
-	size_t imsg = e->imsg;
-	struct history_message *hmsg = history_messages_item(h, imsg);
-	const struct message *msg = hmsg->message;
-
-	size_t intvl0 = hmsg->interval;
-	size_t intvl = intvl0 + 1;
-	hmsg->interval = intvl;
-
-	// update the event queue
-	const double *intvls = history_intervals(h);
-	size_t nintvl = history_interval_count(h);
-	double delta = (intvl < nintvl) ? intvls[intvl] : INFINITY;
-	double tnext = msg->time + delta;
-
-	if (isfinite(tnext)) {
-		e->time = tnext;
-		pqueue_update_top(&h->events);
-	} else {
-		pqueue_pop(&h->events);
-	}
-
-	// update the actors;
-	struct history_actor *actr;
-	size_t ito, nto = msg->nto;
-
-	actr = &h->senders[msg->from];
-	for (ito = 0; ito < nto; ito++) {
-		size_t ix = find_ind(actr, msg->to[ito]);
-		assert(ix < actr->nz);
-		size_t *ptr = actr->nmsg + intvl0 + ix * nintvl;
-		
-		assert(ptr[0]);
-		ptr[0]--;
-
-		if (intvl < nintvl)
-			ptr[1]++;
-	}
-
-	for (ito = 0; ito < nto; ito++) {
-		actr = &h->receivers[msg->to[ito]];
-		size_t ix = find_ind(actr, msg->from);
-		assert(ix < actr->nz);
-		size_t *ptr = actr->nmsg + intvl0 + ix * nintvl;
-		
-		assert(ptr[0]);
-		ptr[0]--;
-
-		if (intvl < nintvl)
-			ptr[1]++;
-	}
-
-#ifndef NDEBUG
-	double time = history_time(h);
-#endif
-
-	//fprintf(stderr, "-> message_advance %p, { %zu" -> [ ", msg, msg->from);
-	//size_t ito;
-
-	//if (msg->nto > 0)
-	//      fprintf(stderr, "%zu", msg->to[0]);
-	//for (ito = 1; ito < msg->nto; ito++) {
-	//      fprintf(stderr, ", %zu", msg->to[ito]);
-	//}
-	//fprintf(stderr, " ] }\n");
-
-	// notify all observers
-	size_t i, n = h->nobs;
-	const struct history_observer *obs;
-	
-	for (i = 0; i < n; i++) {
-		obs = &h->observers[i];
-
-		if (obs->callbacks.message_advance) {
-			obs->callbacks.message_advance(obs->udata, h, msg,
-						       intvl);
-
-			// make sure observers don't add messages
-			// or advance time
-			assert(!has_pending_messages(h));
-			assert(history_time(h) == time);
-		}
-	}
-}
 
 void history_advance(struct history *h, double time)
 {
-	assert(h);
+	size_t imsg, nmsg = h->nmsg;
+	const struct message *msgs = h->msgs;
+	const struct message *msg;
+	struct history_actor *s, *r;
+	size_t ito, nto;
+	size_t from, to;
+	double wt;
+
 	assert(time >= history_time(h));
 
-	if (history_time(h) < time)
-		process_current_messages(h);
+	for (imsg = h->ncur, msg = &msgs[imsg];
+			imsg < nmsg && msg->time <= time; imsg++, msg++) {
+		nto = msg->nto;
+		wt = 1 / (double)nto;
+		from = msg->from;
+		s = history_send(h, from);
+		actor_add_msg(s, imsg);
 
-	while (history_next_time(h) < time) {
-		h->time = history_next_time(h);
-		process_event(h);
+		for (ito = 0; ito < nto; ito++) {
+			to = msg->to[ito];
+			r = history_recv(h, to);
+			actor_add_msg(r, imsg);
+			actor_add_alter(r, from, wt);
+			actor_add_alter(s, to, wt);
+		}
+	}
+	assert(imsg == msg - msgs);
+	h->ncur = imsg;
+	h->tcur = time;
+}
+
+
+void history_add(struct history *h, size_t from, size_t *to, size_t nto,
+		 intptr_t attr)
+{
+	size_t imsg;
+	size_t *to_buf;
+	struct message *msg;
+
+	/* make space for the new message */
+	if (needs_grow(h->nmsg + 1, &h->nmsg_max)) {
+		h->msgs = xrealloc(h->msgs,
+				   h->nmsg_max * sizeof(struct message));
 	}
 
-	assert(history_time(h) <= time);
-	assert(!has_pending_messages(h));
-	assert(history_next_time(h) >= time);
-	h->time = time;
+	/* make space for the new 'to' list */
+	if (needs_grow(h->nto + nto, &h->nto_max)) {
+		h->to = xrealloc(h->to, h->nto_max * sizeof(size_t));
+
+		to_buf = h->to;
+		for (imsg = 0; imsg < h->ncur; imsg++) {
+			h->msgs[imsg].to = to_buf;
+			to_buf += h->msgs[imsg].nto;
+		}
+	} else {
+		if (h->ncur == h->nmsg) {
+			to_buf = h->to + h->nto;
+		} else if (h->ncur) {
+			struct message *prev = &h->msgs[h->ncur - 1];
+			to_buf = prev->to + prev->nto;
+		} else {
+			to_buf = h->to;
+		}
+	}
+
+	/* insert the new message */
+	msg = h->msgs + h->ncur;
+	memmove(msg + 1, msg, (h->nmsg - h->ncur) * sizeof(struct message));
+	msg->from = from;
+	msg->to = to_buf;
+	msg->nto = nto;
+	msg->attr = attr;
+	h->ncur++;
+	h->nmsg++;
+
+	/* insert the new 'to' field */
+	memmove(to_buf + nto, to_buf,
+		(h->nto - (to_buf - h->to)) * sizeof(size_t));
+	memcpy(to_buf, to, nto * sizeof(size_t));
+	to_buf += nto;
+	h->nto += nto;
+
+	/* update the pointers for the subsequent 'to' fields */
+	for (imsg = h->ncur; imsg < h->nmsg; imsg++) {
+		h->msgs[imsg].to = to_buf;
+		to_buf += h->msgs[imsg].nto;
+	}
+	assert(to_buf == h->to + h->nto);
 }
+
 
--- src/history.h
+++ src/history.h
@@ -2,214 +2,94 @@
 #define HISTORY_H
 
 #include <assert.h>
-#include <math.h>
-#include "messages.h"
-#include "pqueue.h"
+#include <stddef.h>
+#include "uintset.h"
 
-struct history {
-	size_t nsend, nrecv;
-	double *intvls;
-	size_t nintvl;
 
+struct message {
 	double time;
-
-	struct history_observer *observers;
-	size_t nobs, nobs_max;
-	struct history_message *msgs;
-	size_t nmsg, nmsg_max;
-	struct history_actor *senders;
-	struct history_actor *receivers;
-	size_t cur_msg;
-	struct pqueue events;
-};
-
-struct history_callbacks {
-	void (*message_add) (void *udata, struct history * h,
-			     const struct message * msg);
-	void (*message_advance) (void *udata, struct history * h,
-				 const struct message * msg, size_t intvl);
-	void (*clear) (void *udata, struct history * h);
-};
-
-struct history_message {
-	const struct message *message;
-	size_t interval;
-};
-
-struct history_event {
-	double time;
-	size_t imsg;
+	size_t from;
+	size_t *to;
+	size_t nto;
+	intptr_t attr;
 };
 
 struct history_actor {
-	size_t *message_ixs;
-	size_t nix, nix_max;
-	size_t *nmsg;
-	size_t *ind;
-	size_t nz, nzmax;
+	struct uintset msgs;
+	struct uintset alters;
+	double *wts;
 };
 
-struct history_observer {
-	void *udata;
-	struct history_callbacks callbacks;
+
+struct history {
+	size_t nsend, nrecv;
+	struct message *msgs;
+	size_t nmsg, nmsg_max;
+	size_t *to;
+	size_t nto, nto_max;
+	struct history_actor *sends;
+	struct history_actor *recvs;
+	double tcur;
+	size_t ncur;
 };
+
 
 /* create/destroy/clear */
-void history_init(struct history *h, size_t nsend, size_t nrecv,
-		const double *intvls, size_t nintvl);
+void history_init(struct history *h, size_t nsend, size_t nrecv);
 void history_deinit(struct history *h);
-void history_clear(struct history *h);
+void history_clear(struct history *h); // delete all messages, reset time
 
 /* properties */
-static inline const double *history_intervals(const struct history *h);
-static inline size_t history_interval_count(const struct history *h);
+#define history_nsend(h) ((h)->nsend)
+#define history_nrecv(h) ((h)->nrecv)
 
+/* access past messages */
+#define history_count(h) ((h)->ncur)
 
-/* time */
-static inline double history_time(const struct history *h);	// current time
-static inline double history_next_time(const struct history *h);	// next change
+static inline struct message *history_item(const struct history *h, size_t i)
+{
+	assert(i < history_count(h));
+	return &h->msgs[i];
+}
+
+/* modify time */
+#define history_time(h) ((h)->tcur)
+void history_reset(struct history *h); // reset time to -INFINITY
 void history_advance(struct history *h, double time);	// advance time
 
-/* messages */
-static inline size_t history_messages_count(const struct history *h);
-static inline struct history_message *history_messages_item(const struct history *h,
-							size_t i);
-void history_add(struct history *h, const struct message *msg);
 
-/* actors */
-static inline size_t history_send_count(const struct history *h);
-static inline size_t history_recv_count(const struct history *h);
-
-static inline void history_get_send_messages(const struct history *h, size_t isend,
-					     const size_t **imsg, size_t *nmsg);
-static inline void history_get_send_active(const struct history *h, size_t isend,
-					   const size_t **jrecv, size_t *nrecv);
-static inline const size_t *history_send_counts(const struct history *h, size_t isend);
-
-static inline void history_get_recv_messages(const struct history *h, size_t irecv,
-					     const size_t **imsg, size_t *nmsg);
-static inline void history_get_recv_active(const struct history *h, size_t jrecv,
-					   const size_t **isend, size_t *nsend);
-static inline const size_t *history_recv_counts(const struct history *h, size_t jrecv);
+/* add messages */
+void history_add(struct history *h, size_t from, size_t *to, size_t nto,
+		 intptr_t attr);
 
 
-
-/* observers */
-void history_add_observer(struct history *h, void *udata,
-			  const struct history_callbacks *callbacks);
-void history_remove_observer(struct history *h, void *udata);
-
-
-/* inline function definitions */
-const double *history_intervals(const struct history *h)
+/* actor histories */
+static inline struct history_actor *history_send(const struct history *h, size_t i)
 {
-	return h->intvls;
+	assert(i < history_nsend(h));
+	return &h->sends[i];
 }
 
-size_t history_interval_count(const struct history *h)
+static inline struct history_actor *history_recv(const struct history *h, size_t j)
 {
-	return h->nintvl;
+	assert(j < history_nrecv(h));
+	return &h->recvs[j];
 }
 
-double history_time(const struct history *h)
+static inline void history_actor_get_msgs(const struct history_actor *ha,
+					  const size_t **ind, size_t *len)
 {
-	assert(h);
-	return h->time;
+	uintset_get_vals(&ha->msgs, ind, len);
 }
 
-double history_next_time(const struct history *h)
+static inline void history_actor_get_alters(const struct history_actor *ha,
+					    const double **wts, const size_t **ind,
+					    size_t *len)
 {
-	assert(h);
-
-	if (pqueue_count(&h->events)) {
-		const struct history_event *e = pqueue_top(&h->events);
-		return e->time;
-	} else {
-		return INFINITY;
-	}
+	uintset_get_vals(&ha->alters, ind, len);
+	*wts = ha->wts;
 }
 
-size_t history_messages_count(const struct history *h)
-{
-	assert(h);
-	return h->nmsg;
-}
 
-struct history_message *history_messages_item(const struct history *h, size_t imsg)
-{
-	assert(h);
-	assert(imsg < history_messages_count(h));
-	return &h->msgs[imsg];
-}
-
-size_t history_send_count(const struct history *h)
-{
-	assert(h);
-	return h->nsend;
-}
-
-size_t history_recv_count(const struct history *h)
-{
-	assert(h);
-	return h->nrecv;
-}
-
-void history_get_send_messages(const struct history *h, size_t isend,
-			       const size_t **imsg, size_t *nmsg)
-{
-	assert(h);
-	assert(isend < history_send_count(h));
-	assert(imsg);
-	assert(nmsg);
-
-	const struct history_actor *a = &h->senders[isend];
-	*imsg = a->message_ixs;
-	*nmsg = a->nix;
-}
-
-void history_get_send_active(const struct history *h, size_t isend, const size_t **jrecv,
-			     size_t *nrecv)
-{
-	const struct history_actor *a = &h->senders[isend];
-	*jrecv = a->ind;
-	*nrecv = a->nz;
-}
-
-const size_t *history_send_counts(const struct history *h, size_t isend)
-{
-	assert(h);
-	assert(isend < history_send_count(h));
-	const struct history_actor *a = &h->senders[isend];
-	return a->nmsg;
-}
-
-void history_get_recv_messages(const struct history *h, size_t irecv,
-			       const size_t **imsg, size_t *nmsg)
-{
-	assert(h);
-	assert(irecv < history_recv_count(h));
-	assert(imsg);
-	assert(nmsg);
-
-	const struct history_actor *a = &h->receivers[irecv];
-	*imsg = a->message_ixs;
-	*nmsg = a->nix;
-}
-
-void history_get_recv_active(const struct history *h, size_t jrecv, const size_t **isend,
-			     size_t *nsend)
-{
-	const struct history_actor *a = &h->receivers[jrecv];
-	*isend = a->ind;
-	*nsend = a->nz;
-}
-
-const size_t *history_recv_counts(const struct history *h, size_t jrecv)
-{
-	assert(h);
-	assert(jrecv < history_recv_count(h));
-	const struct history_actor *a = &h->receivers[jrecv];
-	return a->nmsg;
-}
 
 #endif /* HISTORY_H */
