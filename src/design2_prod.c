ff88be18d0a4ac16e6f62aa7e8e9294c77e13694
Patrick Perry <patperry@gmail.com>
2013-06-09 17:09:07 -0400

    Fix implementation of design2_prod

--- src/design2_prod.c
+++ src/design2_prod.c
@@ -1,32 +1,153 @@
 #include "port.h"
+#include <stdlib.h>
+#include <string.h>
+#include "coreutil.h"
+#include "xalloc.h"
 #include "design2.h"
 
 
-static void prod2_init(struct tvar2 *tv, const char *name, struct history *h, va_list ap);
-static void prod2_deinit(struct tvar2 *tv, struct history *h);
-static void prod2_update_var(void *udata, struct design2 *d, const struct var2 *v, size_t i,
-			     size_t j, const double *delta, const size_t *ind, size_t nz);
 
-static struct design2_callbacks prod2_design_callbacks = {
-	NULL, // update
-	prod2_update_var,
-	NULL // clear
+struct prod2_thunk {
+	const struct var2 *u;
+	const struct var2 *v;
 };
+
+
+
+static void prod2_init(struct var_meta *meta, void **thunk, const char *name, struct design2 *d, va_list ap)
+{
+	const struct var2 *u = va_arg(ap, const struct var2*);
+	const struct var2 *v = va_arg(ap, const struct var2*);
+	size_t rank = u->meta.rank + v->meta.rank;
+	size_t dims[VAR_RANK_MAX];
+
+	memcpy(dims, u->meta.dims, u->meta.rank * sizeof(size_t));
+	memcpy(dims + u->meta.rank, v->meta.dims, v->meta.rank * sizeof(size_t));
+	var_meta_init(meta, VAR_TYPE_TVAR, name, dims, rank);
+
+	struct prod2_thunk *prod = xmalloc(sizeof(*prod));
+	prod->u = u;
+	prod->v = v;
+	*thunk = prod;
+}
+
+
+static void prod2_deinit(struct var_meta *meta, void *thunk, struct design2 *d)
+{
+	struct prod2_thunk *prod = thunk;
+	free(prod);
+	var_meta_deinit(meta);
+}
+
+
+static double *get_x(const struct var2 *v, size_t i, size_t j)
+{
+	const struct design2 *d = v->design;
+	size_t index = v->index;
+	double *x;
+
+	if (v->meta.type == VAR_TYPE_TRAIT) {
+		size_t c = design2_cohort(d, i);
+		size_t n = design2_count2(d);
+		size_t dim = d->trait_dim;
+
+		x = d->trait_x + (c * n + j) * dim + index;
+	} else {
+		size_t off = d->tvar_ir[i];
+		const size_t *ind = d->tvar_jc + off;
+		size_t nz = d->tvar_ir[i+1] - off;
+		ptrdiff_t jz = find_index(j, ind, nz);
+		size_t dim = d->tvar_dim;
+
+		assert(v->meta.type == VAR_TYPE_TVAR);
+
+		if (jz >= 0) {
+			x = d->tvar_x + (off + jz) * dim + index;
+		} else {
+			x = NULL;
+		}
+	}
+
+	return x;
+}
+
+
+static void design2_prod_update(struct design2 *d, struct tvar2 *prod_var,
+				size_t i, size_t j, double t)
+{
+	struct prod2_thunk *prod = prod_var->thunk;
+	const double *u = get_x(prod->u, i, j);
+	const double *v = get_x(prod->v, i, j);
+	size_t nu = prod->u->meta.size;
+	size_t nv = prod->v->meta.size;
+	double *x = design2_make_active(d, prod_var, i, j);
+	size_t nx = prod_var->var.meta.size;
+
+	memset(x, 0, nx * sizeof(double));
+
+	if (u && v && nv) {
+		blas_dger(nv, nu, 1.0, v, 1, u, 1, x, nv);
+	}
+
+	design2_update(d, prod_var, i, j, t);
+}
+
+
+
+
+
+static double prod2_update(struct tvar2 *prod_var, size_t i, double t0, const struct history *h)
+{
+	struct design2 *d = prod_var->var.design;
+	struct prod2_thunk *prod = prod_var->thunk;
+	struct delta *delta;
+
+	if (prod->u->meta.type == VAR_TYPE_TVAR) {
+		struct tvar2 *u = container_of(prod->u, struct tvar2, var);
+		DELTASET_FOREACH(delta, &u->deltaset[i]) {
+			double t = DELTA_TIME(delta);
+			size_t j = DELTA_ITEM(delta);
+
+			if (t < t0)
+				break;
+
+			design2_prod_update(d, prod_var, i, j, t);
+		}
+	}
+
+	if (prod->v->meta.type == VAR_TYPE_TVAR) {
+		struct tvar2 *v = container_of(prod->v, struct tvar2, var);
+		DELTASET_FOREACH(delta, &v->deltaset[i]) {
+			double t = DELTA_TIME(delta);
+			size_t j = DELTA_ITEM(delta);
+
+			if (t < t0)
+				break;
+
+			design2_prod_update(d, prod_var, i, j, t);
+		}
+	}
+
+	return INFINITY; /* time of next update will be determined by u, v */
+}
+
 
 static struct tvar2_type VAR2_PROD2_REP = {
 	prod2_init,
-	prod2_deinit
+	prod2_deinit,
+	prod2_update
 };
 
-static const struct tvar2_type *VAR2_PROD2 = &VAR2_PROD2_REP;
+
+const struct tvar2_type *VAR2_PROD2 = &VAR2_PROD2_REP;
 
 
-const struct var2 *design2_add_prod(struct design2 *d, const char *name, const struct var2 *u, const struct var2 *v)
+
+const struct var2 *design2_add_prod(struct design2 *d, const char *name,
+				    const struct var2 *u, const struct var2 *v)
 {
 	assert(u->design == d);
 	assert(v->design == d);
-	assert(u->meta.size > 0);
-	assert(v->meta.size > 0);
 
 	const struct var2 *res = NULL;
 
@@ -40,127 +161,5 @@ const struct var2 *design2_add_prod(struct design2 *d, const char *name, const s
 }
 
 
-struct prod2_udata {
-	struct design2 *design;
-	const struct var2 *u;
-	const struct var2 *v;
-	double *delta;
-	size_t *ind;
-};
 
-void prod2_init(struct tvar2 *tv, const char *name, struct history *h, va_list ap)
-{
-	struct design2 *d = va_arg(ap, struct design2*);
-	const struct var2 *u = va_arg(ap, const struct var2*);
-	const struct var2 *v = va_arg(ap, const struct var2*);
-
-	size_t rank = u->meta.rank + v->meta.rank;
-	size_t dims[VAR_RANK_MAX];
-	memcpy(dims, u->meta.dims, u->meta.rank * sizeof(dims[0]));
-	memcpy(dims + u->meta.rank, v->meta.dims, v->meta.rank * sizeof(dims[0]));
-	var_meta_init(&tv->var.meta, name, VAR_TYPE_TVAR, dims, rank);
-	assert(tv->var.meta.size == u->meta.size * v->meta.size);
-
-	struct prod2_udata *udata = xmalloc(sizeof(*udata));
-	udata->design = d;
-	udata->u = u;
-	udata->v = v;
-	udata->delta = xmalloc(v->meta.size * sizeof(*udata->delta));
-	udata->ind = xmalloc(v->meta.size * sizeof(*udata->ind));
-	tv->udata = udata;
-
-	design2_add_observer(d, tv, &prod2_design_callbacks);
-
-	(void)h;
-}
-
-
-void prod2_deinit(struct tvar2 *tv, struct history *h)
-{
-	struct prod2_udata *udata = tv->udata;
-	design2_remove_observer(udata->design, tv);
-	free(udata->ind);
-	free(udata->delta);
-	free(udata);
-
-	(void)h;
-}
-
-
-void prod2_update_var(void *udata, struct design2 *d, const struct var2 *v, size_t i,
-		      size_t j, const double *delta, const size_t *ind, size_t nz)
-{
-	assert(ind || !nz);
-	assert(nz <= v->meta.size);
-
-	const struct tvar2 *tv = udata;
-	const struct prod2_udata *udata0 = tv->udata;
-	double *vdelta = udata0->delta;
-	size_t *vind = udata0->ind;
-	size_t vnz;
-	size_t iz;
-
-	if (!nz)
-		return;
-
-	if (v == udata0->u) {
-		const double *dx = delta;
-		const double *y;
-
-		if (udata0->v->meta.type == VAR_TYPE_TRAIT) {
-			y = design2_trait(d, udata0->v, i, j);
-		} else {
-			y = design2_tvar(d, udata0->v, i, j);
-		}
-
-		size_t iy, ny = udata0->v->meta.size;
-
-		if (!y || !ny)
-			return;
-
-		vnz = nz * ny;
-		memset(vdelta, 0, vnz * sizeof(*vdelta));
-		blas_dger(ny, nz, 1.0, y, 1, dx, 1, vdelta, ny);
-
-		size_t *dst = vind;
-
-		for (iz = 0; iz < nz; iz++) {
-			for (iy = 0; iy < ny; iy++) {
-				*dst++ = ind[iz] * ny + iy;
-			}
-		}
-
-	} else if (v == udata0->v) {
-		const double *dy = delta;
-		const double *x;
-
-		if (udata0->u->meta.type == VAR_TYPE_TRAIT) {
-			x = design2_trait(d, udata0->u, i, j);
-		} else {
-			x = design2_tvar(d, udata0->u, i, j);
-		}
-
-		size_t ix, nx = udata0->u->meta.size;
-		size_t ny = udata0->v->meta.size;
-
-		if (!x || !nx)
-			return;
-
-		vnz = nx * nz;
-		memset(vdelta, 0, vnz * sizeof(*vdelta));
-		blas_dger(nz, nx, 1.0, dy, 1, x, 1, vdelta, nz);
-
-		size_t *dst = vind;
-
-		for (ix = 0; ix < nx; ix++) {
-			for (iz = 0; iz < nz; iz++) {
-				*dst++ = ix * ny + ind[iz];
-			}
-		}
-	} else {
-		return;
-	}
-
-	design2_update(d, &tv->var, i, j, vdelta, vind, vnz);
-}
 
