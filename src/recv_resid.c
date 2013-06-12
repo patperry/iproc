1341a985f198d0105a0229c177ceac418b81963f
Patrick Perry <patperry@gmail.com>
2013-06-12 09:27:08 -0400

    Pport recv_resid

--- src/recv_resid.c
+++ src/recv_resid.c
@@ -37,8 +37,7 @@ static void axpy_probs(double alpha, const struct recv_model *m, size_t isend,
 		       double *y)
 {
 	const struct catdist1 *dist = recv_model_dist(m, isend);
-	const struct frame *f = recv_model_frame(m);
-	size_t j, n = frame_recv_count(f);
+	size_t j, n = recv_model_count(m);
 
 	for (j = 0; j < n; j++) {
 		double p = catdist1_prob(dist, j);
@@ -63,11 +62,11 @@ static void update_obs(struct recv_resid_count *obs, const struct message *msg, 
 	obs->tot += nto;
 }
 
+
 static void update_fit(struct recv_resid_count *fit, const struct message *msg,
 		       const struct recv_model *model)
 {
-	const struct frame *f = recv_model_frame(model);
-	size_t nrecv = frame_recv_count(f);
+	size_t nrecv = recv_model_count(model);
 	size_t isend = msg->from;
 	size_t nto = msg->nto;
 
@@ -81,53 +80,59 @@ static void update_fit(struct recv_resid_count *fit, const struct message *msg,
 	axpy_probs(nto, model, isend, row);
 }
 
-static void recv_resid_set(struct recv_resid *resid,
-		           struct frame *f,
-			   const struct messages *msgs,
-			   const struct recv_coefs *coefs)
-{
-	size_t nsend = frame_send_count(f);
-	size_t nrecv = frame_recv_count(f);
-	struct history *h = frame_history(f);
 
-	recv_model_set_coefs(&resid->model, coefs);
+void recv_resid_set(struct recv_resid *resid,
+		    struct recv_model *model,
+		    const struct message *msgs,
+		    size_t nmsg)
+{
+	size_t nsend = recv_model_send_count(model);
+	size_t nrecv = recv_model_count(model);
+	const struct design *r = recv_model_design(model);
+	const struct design2 *d = recv_model_design2(model);
+	struct history *hr = design_history(r);
+	struct history *hd = design2_history(d);
+	size_t imsg;
+
 	recv_resid_clear(&resid->obs, nsend, nrecv);
 	recv_resid_clear(&resid->fit, nsend, nrecv);
 
-	struct messages_iter it;
-	MESSAGES_FOREACH(it, msgs) {
-		size_t i, n = MESSAGES_COUNT(it);
-		double t = MESSAGES_TIME(it);
+	for (imsg = 0; imsg < nmsg; imsg++) {
+		const struct message *msg = &msgs[imsg];
+		double t = msg->time;
 
-		history_advance(h, t);
-		for (i = 0; i < n; i++) {
-			struct message *msg = MESSAGES_VAL(it, i);
-			history_add(h, msg);
-
-			update_obs(&resid->obs, msg, nrecv);
-			update_fit(&resid->fit, msg, &resid->model);
+		if (history_time(hr) < t) {
+			history_reset(hr);
 		}
+
+		history_advance(hr, t);
+		if (history_time(hd) < t) {
+			history_reset(hd);
+		}
+		history_advance(hd, t);
+
+		update_obs(&resid->obs, msg, nrecv);
+		update_fit(&resid->fit, msg, model);
 	}
 }
 
-void recv_resid_init(struct recv_resid *resid,
-		     struct frame *f,
-		     const struct messages *msgs,
-		     const struct recv_coefs *coefs)
-{
-	size_t nsend = frame_send_count(f);
-	size_t nrecv = frame_recv_count(f);
 
-	recv_model_init(&resid->model, f, coefs);
+void recv_resid_init(struct recv_resid *resid,
+		     struct recv_model *model,
+		     const struct message *msgs,
+		     size_t nmsg)
+{
+	size_t nsend = recv_model_send_count(model);
+	size_t nrecv = recv_model_count(model);
+
 	recv_resid_count_init(&resid->obs, nsend, nrecv);
 	recv_resid_count_init(&resid->fit, nsend, nrecv);
 
-	recv_resid_set(resid, f, msgs, coefs);
+	recv_resid_set(resid, model, msgs, nmsg);
 }
 
 void recv_resid_deinit(struct recv_resid *resid)
 {
 	recv_resid_count_deinit(&resid->fit);
 	recv_resid_count_deinit(&resid->obs);
-	recv_model_deinit(&resid->model);
 }
--- src/recv_resid.h
+++ src/recv_resid.h
@@ -1,26 +1,25 @@
 #ifndef RECV_RESID_H
 #define RECV_RESID_H
 
-#include "messages.h"
+#include "history.h"
 #include "recv_model.h"
 
 struct recv_resid_count {
 	double *dyad;
 	double *send;
 	double *recv;
-	size_t tot;
+	double tot;
 };
 
 struct recv_resid {
-	struct recv_model model;
 	struct recv_resid_count obs;
 	struct recv_resid_count fit;
 };
 
 void recv_resid_init(struct recv_resid *resid,
-		     struct frame *f,
-		     const struct messages *msgs,
-		     const struct recv_coefs *coefs);
+		     struct recv_model *model,
+		     const struct message *msgs,
+		     size_t nmsg);
 void recv_resid_deinit(struct recv_resid *resid);
 
 #endif /* RECV_RESID_H */
