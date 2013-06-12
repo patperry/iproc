409e06b70ac9dcfaf027eb9084e5a1b88039ce98
Patrick Perry <patperry@gmail.com>
2013-06-12 18:43:02 -0400

    Remove some code from recv_fit

--- src/recv_fit.h
+++ src/recv_fit.h
@@ -9,31 +9,6 @@
 #include "recv_loglik.h"
 #include "recv_model.h"
 
-#define RECV_FIT_GTOL0		(1e-8)
-#define RECV_FIT_XTOL0		(1e-8)
-#define RECV_FIT_LSMAX0		(10)
-#define RECV_FIT_LSCTRL0	LINESEARCH_CTRL0
-
-#define RECV_FIT_CTRL0 ((struct recv_fit_ctrl) { \
-		RECV_FIT_GTOL0, \
-		RECV_FIT_XTOL0, \
-		RECV_FIT_LSMAX0, \
-		RECV_FIT_LSCTRL0 \
-	})
-
-struct recv_fit_ctrl {
-	double gtol;
-	double xtol;
-	size_t ls_maxit;
-	struct linesearch_ctrl ls;
-};
-
-enum recv_fit_task {
-	RECV_FIT_CONV = 0,
-	RECV_FIT_STEP = 1,
-	RECV_FIT_ERR_LNSRCH = -1,	// linesearch failed to converge
-	RECV_FIT_ERR_XTOL = -2,	// step size is smaller than tolerance
-};
 
 
 struct recv_fit_params {
@@ -55,14 +30,6 @@ struct recv_fit_eval {
 	struct recv_fit_resid resid;
 };
 
-struct recv_fit_kkt {
-	double *matrix;
-	ptrdiff_t *ldl_ipiv;
-	double *ldl_work;
-	double *imat_buf;
-	size_t ldl_lwork;
-	int factored;
-};
 
 struct recv_fit_search {
 	struct recv_fit_params params;
@@ -73,7 +40,6 @@ struct recv_fit_rgrad {
 };
 
 struct recv_fit {
-	struct recv_fit_ctrl ctrl;
 	const struct messages *xmsgs, *ymsgs;
 
 	/* working frame + model */
@@ -83,19 +49,6 @@ struct recv_fit {
 	/* null deviance */
 	double dev0;
 
-	/* optimization constraints */
-	struct constr *constr;
-
-	/* optimization workspace */
-	struct recv_fit_eval eval[2], *cur, *prev;
-	struct recv_fit_kkt kkt;
-	struct recv_fit_search search;
-	struct recv_fit_rgrad rgrad;
-
-	/* additional workspace */
-	//struct linesearch ls;
-	enum recv_fit_task task;
-	double step;
 };
 
 void recv_fit_params_init(struct recv_fit_params *params, const struct frame *f,
@@ -132,21 +85,5 @@ const double *recv_fit_duals(const struct recv_fit *fit);
 double recv_fit_step(const struct recv_fit *fit);
 double recv_fit_grad_norm2(const struct recv_fit *fit);
 
-/* control parameters */
-static inline int recv_fit_ctrl_valid(const struct recv_fit_ctrl *ctrl);
-
-/* inline function definitions */
-int recv_fit_ctrl_valid(const struct recv_fit_ctrl *ctrl)
-{
-	assert(ctrl);
-
-	if (!(ctrl->gtol > 0)) {
-		return 0;
-	} else if (!(ctrl->ls_maxit > 0)) {
-		return 0;
-	} else {
-		return linesearch_ctrl_valid(&ctrl->ls);
-	}
-}
 
 #endif /* RECV_FIT_H */
